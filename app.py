10402a72132226727428b7f81c4312d6b5384544
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-21 17:20:20 -0700

    Complete main functionality

--- app.py
+++ app.py
@@ -11,4 +11,16 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def home_page():
-    return render_template('questions.html')
+    """Returns the homepage"""
+
+    return render_template('questions.html',
+                           prompts=silly_story.prompts)
+
+
+@app.get("/results")
+def results_page():
+    """Returns the result page"""
+
+    story = silly_story.get_result_text(request.args)
+    return render_template("results.html",
+                           interpolated_story=story)
